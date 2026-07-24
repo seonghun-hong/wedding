a009976451b103b089296041eab32f0026fd5978
seonghun <fd6s8stc5m@privaterelay.appleid.com>
2026-07-24 22:49:37 +0900

    모바일 영상과 공유 이미지 개선

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -12,7 +12,7 @@ declare global {
 
 const WEDDING_URL = "https://seonghun-hong.github.io/wedding/";
 const KAKAO_IMAGE_URL =
-  "https://seonghun-hong.github.io/wedding/images/og.jpg?v=20260723";
+  "https://seonghun-hong.github.io/wedding/images/og.jpg?v=20260724";
 
 export function FooterSection() {
   const [toast, setToast] = useState("");
